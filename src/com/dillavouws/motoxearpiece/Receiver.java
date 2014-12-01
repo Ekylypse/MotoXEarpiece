857c0bc28e3ada73887ff9bda533ba1329ca54f0
Anthony Dillavou <ekylypse@gmail.com>
2014-12-01 08:23:09 -0600

    pull arayray changes

--- src/com/dillavouws/motoxearpiece/Receiver.java
+++ src/com/dillavouws/motoxearpiece/Receiver.java
@@ -1,21 +1,16 @@
 package com.dillavouws.motoxearpiece;
 
-/**
- * Created by Anthony on 11/21/2014.
- */
-
-
-import android.content.Intent;
-import android.content.Context;
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
 
-
-public class BootCompletedReceiver extends BroadcastReceiver {
-
-    final static String TAG = "BootCompletedReceiver";
+public class Receiver extends BroadcastReceiver {
 
     public void onReceive(Context context, Intent arg1) {
+    	// START SERVICE
         Intent serviceIntent = new Intent(context, SpeakerFix.class);
+        //Toast.makeText(context, "Called Intercepted", Toast.LENGTH_SHORT).show();
         context.startService(serviceIntent);
     }
 }
---
+++ src/com/dillavouws/motoxearpiece/SpeakerFix.java
@@ -0,0 +1,58 @@
+package com.dillavouws.motoxearpiece;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+
+
+public class SpeakerFix extends Service {
+
+
+    private SpeakerFix mContext;
+
+	@Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+    	
+    	mContext = this;
+    	
+        TelephonyManager TelephonyMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        TelephonyMgr.listen(new TeleListener(),
+                PhoneStateListener.LISTEN_CALL_STATE);
+    }
+
+    class TeleListener extends PhoneStateListener {
+        AudioManager audioManager = (AudioManager)
+                getSystemService(Context.AUDIO_SERVICE);
+        public void onCallStateChanged(int state, String incomingNumber) {
+            super.onCallStateChanged(state, incomingNumber);
+            switch (state) {
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    // CALL_STATE_OFFHOOK,
+                    // TOGGLE SPEAKER
+                	audioManager.setSpeakerphoneOn(true);
+                	audioManager.setSpeakerphoneOn(false);
+                	
+                	// KILL SERVICE AND WAIT FOR NEXT CALL
+                	mContext.stopSelf();
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    @Override
+    public void onDestroy() {
+    }
+}
